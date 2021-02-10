78ea703b82c3576543ebaca071a305ccf7330eeb
tony913 <tonycrosby96@gmail.com>
2021-02-10 14:16:52 -0600

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/BudgetDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/BudgetDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
